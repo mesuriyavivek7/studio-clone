e6d84346b3e7b2fcf7efbc6336aacad18493ec3f
mesuriyavivek7 <vivekmesuriya6@gmail.com>
2024-01-10 22:44:08 +0530

    resp-navbar-complete

--- src/componets/navbar/NavSet.js
+++ src/componets/navbar/NavSet.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import './navbar.scss'
 //import images
@@ -6,20 +6,55 @@ import LOGO from '../../images/logo.png'
 //import { Link } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 
+//import mui icons
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+
 export default function NavSet() {
+  const [mobileMode,setmobileMode]=useState(false);
+  const [bars,setBars]=useState(false);
+  
+  useEffect(()=>{
+    setmobileMode(window.innerWidth<768?true:false)
+  })
+
   return (
     <div className='navbar'>
-         <div className='container'>
+        
              <div className='logo'>
-               <Link tp='/'><img src={LOGO}></img></Link>
+               <Link to='/'><img src={LOGO}></img></Link>
              </div>
-             <div className='menu'>
-               <Link className='link' to='/about'>About</Link>
-               <Link className='link' to='/recentwork'>Recent Work</Link>
-               <Link className='link' to='/youtube'>YT Channel</Link>
-               <Link className='link' to='/contact'>Contact</Link>
-             </div>
-         </div>
+             
+ 
+            {
+              (mobileMode===true)&&
+                (
+                 <div className='bars'>
+                 {
+                  (bars===true)?
+                  (<CloseIcon className='icons' onClick={()=>setBars(!bars)}></CloseIcon>):
+                  (<MenuIcon className='icons' onClick={()=>setBars(!bars)}></MenuIcon>)
+                 }
+                </div>
+               )
+            }
+
+            {
+              (bars===true || mobileMode===false) && (
+                <div className='menu'>
+                  <Link className='link' to='/about'>About</Link>
+                  <Link className='link' to='/recentwork'>Recent Work</Link>
+                  <Link className='link' to='/youtube'>YT Channel</Link>
+                  <Link className='link' to='/contact'>Contact</Link>
+                </div>
+              )
+               
+            }
+            
+
+             
+
+       
     </div>
   )
 }
